70912f9661183e7ee7d39357cd8c6e1503fd318e
berenit <berenit@gmail.com>
2024-07-07 13:26:16 +0200

    Fourth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace Database\migrations\database\seeders;
+namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Seeder;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
